7623a400d7b645e0ee6571e4e874e2fb7f3a770c
Adrian <adrian@adrianthedev.com>
2023-08-18 00:54:15 +0300

    fix typo

--- template/app/avo/cards/users_count.rb
+++ template/app/avo/cards/users_count.rb
@@ -1,7 +1,7 @@
 class Avo::Cards::UsersCount < Avo::Dashboards::MetricCard
   self.id = "users_count"
   self.label = "Users count"
-  self.description = "Total number of users registered"
+  self.description = "Total number of registered users"
 
   def query
     result ::User.all.count
