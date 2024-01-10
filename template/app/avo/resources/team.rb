badc37bc49941cd27bf63f34b69580830be61df1
Julian Cheal <julian.cheal@gmail.com>
2024-01-10 16:27:12 +0000

    Update the Bullet Train template
    Updated the Avo community gem version. Pulling gem from rubygems
    Configured the root path to be consistent with setting up a new
    Bullet Train App
    Changed team resource match the code in the current Bullet Train repo
    
    Fixed UsersCount card https://github.com/avo-hq/avo_upgrade/issues/6

--- template/app/avo/resources/team.rb
+++ template/app/avo/resources/team.rb
@@ -12,13 +12,13 @@ class Avo::Resources::Team < Avo::BaseResource
 
   def fields
     field :id, as: :id
-    field :name, as: :text
-    field :slug, as: :text
-    field :being_destroyed, as: :boolean, hide_on: :forms
-    field :time_zone, as: :text
-    field :locale, as: :text
-    field :users, as: :has_many, through: :memberships
+    field :email, as: :text
+    field :first_name, as: :text
+    field :last_name, as: :text
+    field :time_zone, as: :select, options: -> { view_context.time_zone_options_for_select(Avo::Current.user.time_zone, nil, ActiveSupport::TimeZone) }
+    field :current_team, as: :belongs_to
+
+    field :teams, as: :has_many, through: :teams
     field :memberships, as: :has_many
-    field :invitations, as: :has_many
   end
 end
